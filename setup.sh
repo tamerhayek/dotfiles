7c52f5a3bdafc2134e84df86cbc4ee58b8846bc2
tamerhayek <dev@tamerhayek.com>
2024-06-11 15:39:05 +0200

    feat: backup before syncing dotfiles
    - refactor space between echos

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
-echo "Syncing Dotfiles..."
+echo "Machine Setup"
+
+echo ""
+
+echo "Backing up dotfiles to ~/.dotfiles/backups/data-<time>..."
+BACKUP_TIME=$(date "+%Y_%m_%d_%H%M%S")
+echo "Backing up ~/.dotfiles to ~/.dotfiles/backups/data-$BACKUP_TIME..."
+mkdir -p ~/.dotfiles/backups/data-$BACKUP_TIME
+cp -r ~/.config ~/.dotfiles/backups/data-$BACKUP_TIME
+cp -r ~/.gitconfig ~/.dotfiles/backups/data-$BACKUP_TIME
+cp -r ~/.zshrc ~/.dotfiles/backups/data-$BACKUP_TIME
+cp -r ~/.p10k.zsh ~/.dotfiles/backups/data-$BACKUP_TIME
+cp -r ~/.nvmrc ~/.dotfiles/backups/data-$BACKUP_TIME
+echo "Done!"
 
 echo ""
 
 # Sync dotfiles
+echo "Syncing Dotfiles..."
+
 echo "Copying ~/.dotfiles/data/.config/* to ~/.config..."
 cp -r ~/.dotfiles/data/.config/* ~/.config/
 echo "Done!"
@@ -28,9 +43,13 @@ echo ""
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	# Linux
 	echo "Syncing Linux..."
+
+	echo ""
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	# Mac OSX
 	echo "Syncing MacOS..."
+
+	echo ""
 
 	which -s brew
 	if [[ $? != 0 ]]; then
@@ -42,17 +61,24 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 		brew update
 	fi
 
+	echo ""
+
+	# Install brew packages
 	echo "Installing brew packages..."
 	xargs brew install <~/.dotfiles/brew.txt
 
 	brew autoremove
 	brew cleanup
 
+	echo ""
+
 	# Install Node
 	echo "Install Node from NVM"
 	export NVM_DIR=~/.nvm
 	source ~/.nvm/nvm.sh
 	nvm install
+
+	echo ""
 
 	# Install global npm packages
 	echo "Installing global npm packages..."
