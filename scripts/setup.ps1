9f61b03dba9c1e9bd4d34eb57a4373a42d0368e2
tamerhayek <dev@tamerhayek.com>
2024-12-24 16:48:55 +0100

    feat: add wsl script

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,16 +1,19 @@
-echo "Installing and upgrading apps..."
-
+echo "Upgrade sources..."
 winget source update
 
-winget import $env:USERPROFILE\.dotfiles\deps\winget.json --accept-source-agreements --accept-package-agreements
+echo "Install from $env:USERPROFILE\.dotfiles\deps\winget.json..."
+winget import "$env:USERPROFILE\.dotfiles\deps\winget.json" --accept-source-agreements --accept-package-agreements
 
+echo "Upgrade all.."
 winget upgrade --all
 
 echo "Setting dotfiles..."
-
 ~/.dotfiles/scripts/dotfiles.ps1
 
 echo "Configuring tools..."
 
 echo "Installing vscode-vscodium extensions..."
 cat ~/.dotfiles/deps/vscodium.txt | ForEach-Object { codium --install-extension $_ }
+
+echo "Installing wsl..."
+~/.dotfiles/scripts/wsl.ps1
