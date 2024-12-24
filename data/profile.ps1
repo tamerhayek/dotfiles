beea35c57216dc71535dfdfabaae651d78b7b5b8
tamerhayek <dev@tamerhayek.com>
2024-12-24 18:00:37 +0100

    feat: create symlink for powershell profile

---
+++ data/profile.ps1
@@ -0,0 +1,6 @@
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+
+# fnm
+fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+fnm use $(cat ~/.node-version)
--- scripts/dotfiles.ps1
+++ scripts/dotfiles.ps1
@@ -1,3 +1,6 @@
+echo "Linking $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 to $env:USERPROFILE\.dotfiles\data\profile.ps1..."
+New-Item -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.dotfiles\data\profile.ps1
+
 echo "Linking $env:USERPROFILE\AppData\Roaming\VSCodium\User\settings.json to $env:USERPROFILE\.dotfiles\data\codium\settings.json..."
 New-Item -Path $env:USERPROFILE\AppData\Roaming\VSCodium\User\settings.json -ItemType SymbolicLink -Value $env:USERPROFILE\.dotfiles\data\codium\settings.json
 
